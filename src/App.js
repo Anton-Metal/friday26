8be3ba1dc01a006c75eae36a68db87faa129a7b8
Anton-Metal <anton.melin@iths.se>
2021-11-26 13:39:36 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const query = `
+{
+	blogPostCollection {
+		items {
+		   sys {id}
+		  title
+		}
+	}
+}
+`;
 
 function App() {
+  const [posts, setposts] = useState(null);
+
+  useEffect(() => {
+    window
+      .fetch("https://graphql.contentful.com/content/v1/spaces/" + process.env.REACT_APP_CONTENTFUL_SPACEID + "/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env.REACT_APP_CONTENTFUL_DELIVERY_API_ACCESS_TOKEN,
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        setposts(data.blogPostCollection.items);
+      });
+  }, []);
+
+  if (!posts) {
+    return "Loading...";
+  }
+
+  // render the fetched Contentful data
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+	<header className="App-header">
+	  <h2>Titles</h2>
+	  <p>Here is a list of the titles of my contentful posts!</p>
+	  <ul>
+		{posts.map(post => {
+		  return <li key={post.sys.id}>{post.title}</li>
+		})}
+	  </ul>
+	</header>
+  </div>
   );
 }
 
